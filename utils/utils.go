891e0205020385386279f8681a0ed32fa6913cca
zibul444 <granj444@mail.ru>
2019-02-10 20:54:05 +0300

    ref

--- utils/utils.go
+++ utils/utils.go
@@ -13,20 +13,20 @@ import (
 )
 
 // fixme Необходимо проинициализировать до вызова(подумать о синглетоне на менднях)
-var C redis.Conn
+var Conn redis.Conn
 
 func init() {
-	C = NewPool().Get()
+	Conn = NewPool().Get()
 }
 
-func GetConfig(configName string) (config resources.Config) {
-	configString := ReadFileConfig(configName)
+func GetConfig(configPath string) (config resources.Config) {
+	configString := ReadFileConfig(configPath)
 	return UnmarshalConfig(configString)
 }
 
-// читаем файл по имени
-func ReadFileConfig(fileName string) (result string) {
-	f, err := os.Open(fileName)
+// Читаем файл по имени
+func ReadFileConfig(filePath string) (fileContents string) {
+	f, err := os.Open(filePath)
 	if err != nil {
 		log.Println(err.Error())
 		os.Exit(1)
@@ -39,7 +39,7 @@ func ReadFileConfig(fileName string) (result string) {
 			break // выходим из цикла
 		}
 		//fmt.Print(string(data[:n]))
-		result += string(buf[:n])
+		fileContents += string(buf[:n])
 	}
 	fmt.Println("End Reading file config")
 	//fmt.Printf("Data from the file: \n%v", result)
@@ -57,9 +57,9 @@ func UnmarshalConfig(marshal string) (config resources.Config) {
 	return
 }
 
-// Для выполнения любых команд не возвращающих строковое значение
+// Для выполнения любых команд
 func ExecuteCommand(commandName string, args ...interface{}) interface{} {
-	result, err := C.Do(commandName, args...)
+	result, err := Conn.Do(commandName, args...)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -67,9 +67,9 @@ func ExecuteCommand(commandName string, args ...interface{}) interface{} {
 	return result
 }
 
-// Для выполнения команд возвращающих строку
+// Для выполнения команд "get string"
 func Execute(commandName string, args ...interface{}) string {
-	result, err := C.Do(commandName, args...)
+	result, err := Conn.Do(commandName, args...)
 	if err != nil {
 		fmt.Println(err)
 	}
@@ -92,18 +92,18 @@ func NewPool() *redis.Pool {
 }
 
 // Рандомное значение от min до max включительно.
-func GetRandom(min int, max int) int {
+func GetRandom(min int, max int) (r int) {
 	max++
 	rand.Seed(time.Now().UnixNano())
 	//maxp := flag.Int("max", max-min, "the max value")
 
-	n := rand.Intn(max - min)
-	n += min
-	return n
+	r = rand.Intn(max - min)
+	r += min
+	return r
 }
 
 // Получаем случайный ресурс из config
-func GetRandomUrl(config resources.Config) string {
+func GetRandomUrl(config resources.Config) (url string) {
 	min, max := 0, len(config.URLs)
 	max--
 	r := GetRandom(min, max)
@@ -112,7 +112,7 @@ func GetRandomUrl(config resources.Config) string {
 }
 
 // Получаем случайное время жизни впределах указанных в config
-func GetRandomTimeLife(config resources.Config) int {
+func GetRandomTimeLife(config resources.Config) (timeLife int) {
 	min, max := config.MinTimeout, config.MaxTimeout
 
 	return GetRandom(min, max)
