f1bb21ee94a7b7b87c9fd27e77e5a0493cf13a73
zibul444 <granj444@mail.ru>
2019-02-11 22:01:41 +0300

    edit server

--- cacher/server/main.go
+++ cacher/server/main.go
@@ -5,10 +5,14 @@ package main
 
 import (
 	"context"
+	"fmt"
+	"gRPC-Cache/utils"
 	"log"
 	"net"
+	"net/http"
+	"time"
 
-	pb "gRPC-Cache/cacher/description"
+	pb "gRPC-Cache/cacher/description" //fixme забирать pd.go из ../resources
 
 	"google.golang.org/grpc"
 )
@@ -17,13 +21,48 @@ const (
 	port = "localhost:9999"
 )
 
+var (
+	timeLife int
+	url      string
+)
+
 // описание используется для реализации description.serviceCacher
 type server struct{}
 
 func (s *server) GetRandomDataStream(ctx context.Context, in *pb.Request) (*pb.Reply, error) {
-	//log.Printf("Received: %v", in.Name)
-	//log.Printf("Received: %s", in.String())
 	log.Println("Received")
+
+	conf := utils.GetConfig("resources/config.yml") // получаем конфиг
+	url = utils.GetRandomUrl(conf)                  // получаем случайный ресурс из конфига
+	keys := utils.GetListReadyKeys()                // получаем список доступных лючей
+
+	if len(keys) > 0 {
+		for _, k := range keys {
+			fmt.Println(k)
+		}
+	}
+
+	resp, err := http.Get(url) // получаем данные
+	defer resp.Body.Close()    // утилизируем ресурсы
+	if err != nil {
+		log.Fatalf("Error: %v", err)
+	}
+	data := fmt.Sprint(resp)
+	//log.Println(resp)
+	log.Println(data[:100])
+
+	timeLife = utils.GetRandomTimeLife(conf)
+	utils.ExecuteCommand("SETEX", url, timeLife, resp)
+	utils.Execute("GET", url)
+
+	url = utils.GetRandomUrl(conf)
+	resp, err = http.Get(url)
+
+	utils.Execute("KEYS", "*")
+
+	time.Sleep(100 * time.Millisecond)
+
+	//log.Printf("Received: %s", in._())
 	return &pb.Reply{Data: "Hello " + "World"}, nil
 }
 
